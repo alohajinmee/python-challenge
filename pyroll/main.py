a5dbb7bade23ee3cc9386733a3658c7b44c91c3b
alohajinmee <victoriajinmeero@gmail.com>
2024-01-22 18:20:22 -0500

    forgot to save my comments

--- pyroll/main.py
+++ pyroll/main.py
@@ -2,3 +2,8 @@ import os
 import path
 
 csv_file_path = os.path.join("resources", "election_data.csv")
+
+# Same as above just using Pandas library to import our path 
+# import pandas as pd
+
+# file_path = pd.read_csv("Resources/election_data.csv")
